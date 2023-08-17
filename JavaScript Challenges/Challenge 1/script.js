a9cf2a01b30191fd2587233f075e56e7d395cd78
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-08-17 11:10:01 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,4 +1,21 @@
 console.log('I am linked now!');
 
 const name = "mohammad";
-console.log(name);
+alert(name)
+
+const String = "this is a text";
+console.log(String  + " the data type is String");
+const bool1 = "true";
+console.log(bool1  +  " the data type is boolean");
+const bool2 = "false";
+console.log(bool2  +   " the data type is boolean");
+const number = "1215";
+console.log(number  +  " the data type is number");
+const string = "999";
+console.log(string  +  " the data type is String");
+const operator = `{}`;
+console.log(operator  +  " the data type is Operator" );
+const array= "[]";
+console.log(array  +  " the data type is Array" );
+const per="null undefined";
+ console.log(per  +  " the data type is primitive data types")

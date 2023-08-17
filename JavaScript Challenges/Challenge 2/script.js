043ba4eaeac4a394d5cec340614f95a27d06c7db
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-08-17 15:05:58 +0300

    Challenge 2 - Done with Step 2

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -1,7 +1,7 @@
 console.log("hello world!");
 // step 1
-let age = 20;
-if ( age >= 20){
+let age0 = 20;
+if ( age0 >= 20){
     console.log("You can enter this room.")
 }else{
     console.log("You can't enter this room.")
@@ -14,3 +14,49 @@ const expression3 = true && false;
 const expression4 = !expression3;
 
 console.log(expression1, expression2, expression3, expression4);
+
+// step 3
+
+const age = 25;
+let hasDriverLicense = true;
+// hasDriverLicense = !hasDriverLicense
+let hasCar = false;
+let canDrive = false;
+let canRentCar = false;
+let needDriver = false;
+
+
+
+if (age >= 18 && hasDriverLicense) {
+  canDrive = true;
+}
+
+console.log(canDrive);
+
+if (age >= 22 && hasDriverLicense && !hasCar) {
+    canRentCar = true;
+  }
+  
+  console.log(canRentCar);
+
+  if (age < 18 && !hasDriverLicense && !hasCar) {
+    needDriver = true;
+  }
+  console.log(needDriver);
+
+//   STEP 4
+
+const role= "admin";
+switch (role) {
+    case "admin":
+      console.log("You have read and write access for the data");
+        break;
+ case "user":
+ console.log("You have only read access for the data");
+ break;
+ case "guest":
+    console.log("You have no access for the data");
+    default:
+        console.log("User role is not recoginized")
+        break;
+ }

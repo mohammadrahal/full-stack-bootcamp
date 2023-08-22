b45b5b932a744ad539edea7464ca3bb9e2e036eb
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-08-22 15:41:30 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,6 +1,6 @@
 console.log("hello wolrd!")
 
-// step one
+// step 1
 const array = [1,2,3,4,5,6,7];
 console.log(array)
 
@@ -17,3 +17,28 @@ console.log( personalInfo.age + " years");
 console.log("I'm " + personalInfo.height);
 console.log("location " + personalInfo.location);
 console.log("from " + personalInfo.nationality);
+
+// step 2
+// New Array with Spread
+const fiveNumbers = [1, 2, 3,];
+const nextThreeNumbers = [4,5,6];
+
+const allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+
+console.log(allNumbers);
+
+
+// New Object with Spread
+
+const additionalInfo = {
+    occupation: "developer",
+    hobby: "reading",
+    education: "MIS"
+};
+
+const fullInfo = { ...personalInfo, ...additionalInfo };
+const fullInfoArray = Object.entries(fullInfo);
+fullInfoArray.sort((a, b) => a[0].localeCompare(b[0]));
+const sortedFullInfo = Object.fromEntries(fullInfoArray);
+
+console.log(fullInfo);

676b51a7b30635b408d365f89e75dfec6af7336d
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-08-22 15:08:33 +0300

    challange 4 - done with step 1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,19 @@
+console.log("hello wolrd!")
+
+// step one
+const array = [1,2,3,4,5,6,7];
+console.log(array)
+
+const personalInfo = {
+    fname:"mohammad",
+    age:27,
+    height:"185 cm",
+    location:"lebanon tyre",
+    nationality:"lebanon"
+}
+
+console.log("I'm " + personalInfo.fname);
+console.log( personalInfo.age + " years");
+console.log("I'm " + personalInfo.height);
+console.log("location " + personalInfo.location);
+console.log("from " + personalInfo.nationality);
